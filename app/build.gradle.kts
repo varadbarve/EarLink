31c4b426db161aa23de157810b162071d5bd25f6
Varad Barve <varadvbarve@gmail.com>
2026-05-21 20:57:09 +0530

    ktlin::gradle::jva  fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.compose.compiler)
   alias(libs.plugins.kotlin.serialization)
+  alias(libs.plugins.legacy.kapt)
 }
 
 android {
@@ -92,5 +93,5 @@ dependencies {
 
   // Room Database
   implementation(libs.androidx.room.runtime)
-  annotationProcessor(libs.androidx.room.compiler)
+  kapt(libs.androidx.room.compiler)
 }
